154fc1b5a296165a10422970b07281fc53b68d92
Rajiv Seelam <rajiv@betalectic.com>
2021-11-22 15:46:33 +0530

    Make changes for build

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: ['@babel/preset-env', '@babel/preset-flow', '@babel/preset-react'],
+  plugins: ['@babel/plugin-proposal-export-default-from'],
 };
